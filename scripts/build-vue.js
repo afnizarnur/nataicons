c89271f438267425edd144bbdf20424578faf38d
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2020-10-13 21:51:15 +0700

    Update vue component template

--- scripts/build-vue.js
+++ scripts/build-vue.js
@@ -11,9 +11,36 @@ function svgToVue(svg) {
   }).code
 }
 
+const componentTemplate = (name, svg) =>
+  `
+export default {
+  name: '${name}',
+  
+  props: {
+    size: {
+      type: String,
+      default: '24',
+      validator: (s) => (!isNaN(s) || s.length >= 2 && !isNaN(s.slice(0, s.length -1)) && s.slice(-1) === 'x' )
+    }
+  },
+  functional: true,
+  render(h, ctx) {
+    const size = ctx.props.size.slice(-1) === 'x' 
+      ? ctx.props.size.slice(0, ctx.props.size.length -1) + 'em'
+      : parseInt(ctx.props.size) + 'px';
+    const attrs = ctx.data.attrs || {}
+    attrs.width = attrs.width || size
+    attrs.height = attrs.height || size
+    ctx.data.attrs = attrs
+  
+    return ${svg.replace(/<svg([^>]+)>/, "<svg$1 {...ctx.data}>")}
+  }
+}
+`.trim()
+
 console.log("Building Vue components...")
 
-rimraf("./vue/*")
+rimraf(".vue/*")
   .then(() => {
     return Promise.all([
       fs.readdir("./icons").then((files) => {
@@ -22,7 +49,12 @@ rimraf("./vue/*")
             return fs
               .readFile(`./icons/${file}`, "utf8")
               .then((content) => {
-                return svgToVue(content)
+                return componentTemplate(
+                  `${camelcase(file.replace(/\.svg$/, ""), {
+                    pascalCase: true,
+                  })}.js`,
+                  content
+                )
               })
               .then((component) => {
                 const fileName = `${camelcase(file.replace(/\.svg$/, ""), {
