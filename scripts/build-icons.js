9b81bed852c047e927bee51d81241c11d867e924
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2020-10-15 10:03:07 +0700

    feat: add support getting icons 20x20

---
+++ scripts/build-icons.js
@@ -0,0 +1,29 @@
+const { getFigmaAssets } = require("figma-assets-generator")
+
+const get24px = {
+  personalAccessToken: process.env.FIGMA_TOKEN,
+  fileId: "N4TZ7vR8BUDHmlgHMmpp9Q",
+  documentId: "0:1",
+  fileExtension: "svg",
+  output: "icons/24x24",
+}
+
+const get20px = {
+  personalAccessToken: process.env.FIGMA_TOKEN,
+  fileId: "N4TZ7vR8BUDHmlgHMmpp9Q",
+  documentId: "54:135",
+  fileExtension: "svg",
+  output: "icons/20x20",
+}
+
+async function main() {
+  console.log("Get icon 24x24...")
+  await getFigmaAssets(get24px)
+
+  console.log("\nGet icon 20x20...")
+  await getFigmaAssets(get20px)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
